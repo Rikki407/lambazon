09cede82962c8bd3ec4ffd002b304c0002ab15cf
Rishab Lamba <rishablamba407@gmail.com>
2020-10-30 22:01:07 +0530

    header UI render v1

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import amazon_logo from '../../amazon-logo.png';
+import { ShoppingBasket } from '@material-ui/icons';
+
+const Header = () => {
+    return (
+        <>
+            <div className="header">
+                <img src={amazon_logo} alt="" className="header__logo" />
+
+                <div className="header__search">
+                    <input type="text" className="header__searchInput" />
+                    <SearchIcon className="header__searchIcon" />
+                </div>
+                <div className="header__nav">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">
+                            Hello Guest
+                        </span>
+                        <span className="header__optionLineTwo">Sign In</span>
+                    </div>
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Returns</span>
+                        <span className="header__optionLineTwo">& Orders</span>
+                    </div>
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Your</span>
+                        <span className="header__optionLineTwo">Prime</span>
+                    </div>
+                    <div className="header__optionBasket">
+                        <ShoppingBasket />
+                        <span className="header__optionLineTwo header__basketCount">
+                            0
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
