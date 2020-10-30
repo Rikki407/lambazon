53e54ca390dd7b12b972db3d04062492abecf49e
Rishab Lamba <rishablamba407@gmail.com>
2020-10-30 16:30:34 +0530

    app init

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyAy9COzFUSmJ_8x9gE8SAKTK2rBfUrtic8',
+    authDomain: 'lambazon-407.firebaseapp.com',
+    databaseURL: 'https://lambazon-407.firebaseio.com',
+    projectId: 'lambazon-407',
+    storageBucket: 'lambazon-407.appspot.com',
+    messagingSenderId: '749228285061',
+    appId: '1:749228285061:web:3f50c9dcb5af6ec6e7ddec',
+    measurementId: 'G-KWE7RNT5H5',
+};
